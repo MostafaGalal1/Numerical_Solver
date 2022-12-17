9e95858d28d7dc9d18a20538293be30c535ade39
MostafaGalal1 <mostafag649.mg@gmail.com>
2022-12-17 22:42:25 +0200

    adding significant figures (not perfect) enhancing gui adding decomposition dropbox

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import ast
+import time
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QVBoxLayout
 import ctypes
 
 my_app_id = "mycompany.myproduct.subproduct.version"
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
+
+
+class Runtime_Calculate:
+    start_time = 0.0
+    end_time = 0.0
+
+    def set_start_time(self):
+        self.start_time = time.process_time()
+
+    def set_end_time(self):
+        self.end_time = time.process_time()
+
+    def show_runtime(self):
+        print('%.16f' % ((self.end_time * self.end_time - self.start_time * self.start_time) / (1000 * (self.end_time + self.start_time))), "millisecond")
+        self.start_time = 0.0
+        self.end_time = 0.0
+
 
 class Ui_MainWindow(object):
     epsilon = 1e-9
@@ -23,59 +41,84 @@ class Ui_MainWindow(object):
 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButton.setGeometry(QtCore.QRect(30, 400, 95, 20))
-        self.radioButton.setObjectName("radioButton")
-        self.radioButton_2 = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButton_2.setGeometry(QtCore.QRect(180, 400, 95, 20))
-        self.radioButton_2.setObjectName("radioButton_2")
-        self.radioButton_3 = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButton_3.setGeometry(QtCore.QRect(330, 400, 95, 20))
-        self.radioButton_3.setObjectName("radioButton_3")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(20, 400, 411, 22))
-        self.lineEdit.setObjectName("lineEdit")
-        self.plainTextEdit_2 = QtWidgets.QLabel(self.centralwidget)
-        self.plainTextEdit_2.setEnabled(True)
-        self.scroll_area = QtWidgets.QScrollArea(self.centralwidget)
-        self.scroll_area.setGeometry(QtCore.QRect(20, 470, 411, 0))
-        self.scroll_area.setFixedWidth(411)
-        self.scroll_area.setWidget(self.plainTextEdit_2)
+
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.plainTextEdit_2.sizePolicy().hasHeightForWidth())
-        self.plainTextEdit_2.setSizePolicy(sizePolicy)
-        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
-        self.plainTextEdit_2.setMargin(5)
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.plainTextEdit.setGeometry(QtCore.QRect(20, 140, 411, 221))
-        self.plainTextEdit.setObjectName("plainTextEdit")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(120, 30, 191, 81))
-        self.label.setTextFormat(QtCore.Qt.AutoText)
-        self.label.setObjectName("label")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(30, 370, 101, 20))
-        self.label_3.setObjectName("label_3")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(30, 110, 101, 20))
-        self.label_2.setObjectName("label_2")
-        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(20, 10, 411, 22))
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(30, 370, 101, 20))
-        self.label_4.setObjectName("label_4")
+
+        self.main_combobox = QtWidgets.QComboBox(self.centralwidget)
+        self.main_combobox.setGeometry(QtCore.QRect(20, 10, 411, 22))
+        self.main_combobox.setObjectName("main_combobox")
+        self.main_combobox.addItem("")
+        self.main_combobox.addItem("")
+        self.main_combobox.addItem("")
+        self.main_combobox.addItem("")
+        self.main_combobox.addItem("")
+
+        self.info_label = QtWidgets.QLabel(self.centralwidget)
+        self.info_label.setGeometry(QtCore.QRect(120, 30, 191, 81))
+        self.info_label.setTextFormat(QtCore.Qt.AutoText)
+        self.info_label.setObjectName("info_label")
+
+        self.linear_system_label = QtWidgets.QLabel(self.centralwidget)
+        self.linear_system_label.setGeometry(QtCore.QRect(30, 110, 101, 20))
+        self.linear_system_label.setObjectName("linear_system_label")
+
+        self.input_textbox = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.input_textbox.setGeometry(QtCore.QRect(20, 140, 411, 221))
+        self.input_textbox.setObjectName("input_textbox")
+
+        self.pivoting_type_label = QtWidgets.QLabel(self.centralwidget)
+        self.pivoting_type_label.setGeometry(QtCore.QRect(30, 370, 101, 20))
+        self.pivoting_type_label.setObjectName("pivoting_type_label")
+
+        self.decomposition_label = QtWidgets.QLabel(self.centralwidget)
+        self.decomposition_label.setGeometry(QtCore.QRect(30, 370, 120, 20))
+        self.decomposition_label.setObjectName("decomposition_label")
+
+        self.initial_guess_label = QtWidgets.QLabel(self.centralwidget)
+        self.initial_guess_label.setGeometry(QtCore.QRect(30, 370, 101, 20))
+        self.initial_guess_label.setObjectName("initial_guess_label")
+
+        self.none_pivoting = QtWidgets.QRadioButton(self.centralwidget)
+        self.none_pivoting.setGeometry(QtCore.QRect(30, 400, 95, 20))
+        self.none_pivoting.setObjectName("none_pivoting")
+
+        self.partial_pivoting = QtWidgets.QRadioButton(self.centralwidget)
+        self.partial_pivoting.setGeometry(QtCore.QRect(180, 400, 95, 20))
+        self.partial_pivoting.setObjectName("partial_pivoting")
+
+        self.complete_pivoting = QtWidgets.QRadioButton(self.centralwidget)
+        self.complete_pivoting.setGeometry(QtCore.QRect(330, 400, 95, 20))
+        self.complete_pivoting.setObjectName("complete_pivoting")
+
+        self.initial_guess_textbox = QtWidgets.QLineEdit(self.centralwidget)
+        self.initial_guess_textbox.setGeometry(QtCore.QRect(20, 400, 411, 22))
+        self.initial_guess_textbox.setObjectName("initial_guess_textbox")
+
+        self.decomposition_combobox = QtWidgets.QComboBox(self.centralwidget)
+        self.decomposition_combobox.setGeometry(QtCore.QRect(20, 400, 411, 22))
+        self.decomposition_combobox.setObjectName("decomposition_combobox")
+        self.decomposition_combobox.addItem("")
+        self.decomposition_combobox.addItem("")
+        self.decomposition_combobox.addItem("")
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.pressed.connect(self.solve_it)
         self.pushButton.setGeometry(QtCore.QRect(20, 430, 411, 28))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.pressed.connect(self.solve_it)
+
+        self.result_label = QtWidgets.QLabel(self.centralwidget)
+        self.result_label.setEnabled(True)
+        self.result_label.setSizePolicy(sizePolicy)
+        self.result_label.setObjectName("result_label")
+        self.result_label.setMargin(5)
+
+        self.scroll_area = QtWidgets.QScrollArea(self.centralwidget)
+        self.scroll_area.setGeometry(QtCore.QRect(20, 470, 411, 0))
+        self.scroll_area.setFixedWidth(411)
+        self.scroll_area.setWidget(self.result_label)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 451, 26))
@@ -86,51 +129,85 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.comboBox.currentIndexChanged['QString'].connect(self.change)
+        self.main_combobox.currentIndexChanged['QString'].connect(self.change)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.runtime = Runtime_Calculate()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Numerical Solver"))
-        self.radioButton.setText(_translate("MainWindow", "None"))
-        self.radioButton_2.setText(_translate("MainWindow", "Partial"))
-        self.radioButton_3.setText(_translate("MainWindow", "Complete"))
-        self.radioButton.setChecked(True)
 
-        self.label.setText(_translate("MainWindow", "Enter Equations line by line like\n"
-                                                                "3, 9, 16\n"
-                                                                "5, 12, 1"))
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_3.setText(_translate("MainWindow", "Initial guess:"))
-        self.label_2.setText(_translate("MainWindow", "Linear system:"))
-        self.comboBox.setItemText(0, _translate("MainWindow", "gauss elimination"))
-        self.comboBox.setItemText(1, _translate("MainWindow", "gauss-jordan"))
-        self.comboBox.setItemText(2, _translate("MainWindow", "LU-decomposition"))
-        self.comboBox.setItemText(3, _translate("MainWindow", "jacobi"))
-        self.comboBox.setItemText(4, _translate("MainWindow", "gauss-seidel"))
-        self.label_4.setText(_translate("MainWindow", "Pivoting type:"))
+        self.none_pivoting.setText(_translate("MainWindow", "None"))
+        self.partial_pivoting.setText(_translate("MainWindow", "Partial"))
+        self.complete_pivoting.setText(_translate("MainWindow", "Complete"))
+        self.none_pivoting.setChecked(True)
+
+        self.info_label.setText(_translate("MainWindow", "Enter Equations line by line like\n"
+                                                    "3, 9, 16\n"
+                                                    "5, 12, 1"))
+        self.info_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.linear_system_label.setText(_translate("MainWindow", "Linear system:"))
+        self.pivoting_type_label.setText(_translate("MainWindow", "Pivoting type:"))
+        self.initial_guess_label.setText(_translate("MainWindow", "Initial guess:"))
+        self.decomposition_label.setText(_translate("MainWindow", "Decomposition form:"))
+
+        self.main_combobox.setItemText(0, _translate("MainWindow", "gauss elimination"))
+        self.main_combobox.setItemText(1, _translate("MainWindow", "gauss-jordan"))
+        self.main_combobox.setItemText(2, _translate("MainWindow", "LU-decomposition"))
+        self.main_combobox.setItemText(3, _translate("MainWindow", "jacobi"))
+        self.main_combobox.setItemText(4, _translate("MainWindow", "gauss-seidel"))
+
+        self.decomposition_combobox.setItemText(0, _translate("MainWindow", "doolittle form"))
+        self.decomposition_combobox.setItemText(1, _translate("MainWindow", "crout form"))
+        self.decomposition_combobox.setItemText(2, _translate("MainWindow", "cholesky form"))
+
         self.pushButton.setText(_translate("MainWindow", "Solve"))
-        self.label_3.hide()
-        self.lineEdit.hide()
+
+        self.initial_guess_label.hide()
+        self.initial_guess_textbox.hide()
+
+        self.decomposition_label.hide()
+        self.decomposition_combobox.hide()
 
     def change(self):
-        if self.comboBox.currentIndex() == 0 or self.comboBox.currentIndex() == 1:
-            self.label_4.show()
-            self.radioButton.show()
-            self.radioButton_2.show()
-            self.radioButton_3.show()
-            self.label_3.hide()
-            self.lineEdit.hide()
-        elif self.comboBox.currentIndex() == 3 or self.comboBox.currentIndex() == 4:
-            self.label_3.show()
-            self.lineEdit.show()
-            self.label_4.hide()
-            self.radioButton.hide()
-            self.radioButton_2.hide()
-            self.radioButton_3.hide()
+        if self.main_combobox.currentIndex() == 0 or self.main_combobox.currentIndex() == 1:
+            self.pivoting_type_label.show()
+            self.none_pivoting.show()
+            self.partial_pivoting.show()
+            self.complete_pivoting.show()
+
+            self.initial_guess_label.hide()
+            self.initial_guess_textbox.hide()
+
+            self.decomposition_label.hide()
+            self.decomposition_combobox.hide()
+        elif self.main_combobox.currentIndex() == 3 or self.main_combobox.currentIndex() == 4:
+            self.initial_guess_label.show()
+            self.initial_guess_textbox.show()
+
+            self.pivoting_type_label.hide()
+            self.none_pivoting.hide()
+            self.partial_pivoting.hide()
+            self.complete_pivoting.hide()
+
+            self.decomposition_label.hide()
+            self.decomposition_combobox.hide()
+        else:
+            self.decomposition_label.show()
+            self.decomposition_combobox.show()
+
+            self.pivoting_type_label.hide()
+            self.none_pivoting.hide()
+            self.partial_pivoting.hide()
+            self.complete_pivoting.hide()
+
+            self.initial_guess_label.hide()
+            self.initial_guess_textbox.hide()
+
 
     def solve_it(self, epsilon=epsilon):
-        coff = self.plainTextEdit.toPlainText().split('\n')
+        coff = self.input_textbox.toPlainText().split('\n')
         if coff[0].find(',') == -1:
             coff[0] += ','
 
@@ -147,7 +224,7 @@ class Ui_MainWindow(object):
                 if n == 1:
                     row_size = len(a[0])
                 if row_size != len(a[n - 1]):
-                    self.plainTextEdit_2.setText(
+                    self.result_label.setText(
                         f"Row(1) contains {row_size + 1} elements and row({n}) contains {len(a[n - 1]) + 1}"
                         " elements, So your input is invalid")
                     return
@@ -155,34 +232,36 @@ class Ui_MainWindow(object):
                 continue
 
         if n < row_size:
-            self.plainTextEdit_2.setText(
+            self.result_label.setText(
                 f"The number of rows({n}) should be one less than the number of columns({row_size + 1})")
             return
         elif n > row_size:
-            self.plainTextEdit_2.setText(f"Matrix size must be {row_size + 1}x{row_size + 2}")
+            self.result_label.setText(f"Matrix size must be {row_size + 1}x{row_size + 2}")
             return
 
-        if self.comboBox.currentText() == "jacobi" or self.comboBox.currentText() == "gauss-seidel":
+        if self.main_combobox.currentText() == "jacobi" or self.main_combobox.currentText() == "gauss-seidel":
             initial = [0 for _ in range(n)]
             try:
-                row_size = len(list(ast.literal_eval(self.lineEdit.text())))
+                row_size = len(list(ast.literal_eval(self.initial_guess_textbox.text())))
             except:
                 row_size = 0
             if row_size == n:
-                initial = list(ast.literal_eval(self.lineEdit.text()))
+                initial = list(ast.literal_eval(self.initial_guess_textbox.text()))
 
-        if self.comboBox.currentText() == "gauss elimination":
+        if self.main_combobox.currentText() == "gauss elimination":
             gauss_elimination(self, n, a, b)
-        elif self.comboBox.currentText() == "gauss-jordan":
+        elif self.main_combobox.currentText() == "gauss-jordan":
             gauss_jordan(self, n, a, b)
-        elif self.comboBox.currentText() == "jacobi":
-            jacobi(n, a, b, initial, epsilon, 100)
-        elif self.comboBox.currentText() == "gauss-seidel":
-            gauss_seidel(n, a, b, initial, epsilon, 100)
+        elif self.main_combobox.currentText() == "jacobi":
+            jacobi(self, n, a, b, initial, epsilon, 100)
+        elif self.main_combobox.currentText() == "gauss-seidel":
+            gauss_seidel(self, n, a, b, initial, epsilon, 100)
 
-        s = "\n".join(str(" ".join(str(itt) for itt in a[it])) + " " + str(b[it]) for it in range(n))
-        self.plainTextEdit_2.setText(s)
-        self.plainTextEdit_2.adjustSize()
+        significant_figures = 3
+        s = "\n".join(str(" ".join(str(float(f'%.{significant_figures}g' % itt)) for itt in a[it])) + " " + str(b[it]) for it in range(n))
+
+        self.result_label.setText(s)
+        self.result_label.adjustSize()
         self.scroll_area.adjustSize()
         MainWindow.setFixedHeight(self.scroll_area.height() + self.scroll_area.y() + 26)
 
@@ -225,9 +304,9 @@ def complete_pivoting(n, a, b, k, o):
 
 def forward_elimination(self, n, a, b, o):
     for k in range(n):
-        if self.radioButton_2.isChecked():
+        if self.partial_pivoting.isChecked():
             partial_pivoting(n, a, b, k)
-        elif self.radioButton_3.isChecked():
+        elif self.complete_pivoting.isChecked():
             complete_pivoting(n, a, b, k, o)
         if a[k][k] == 0:
             return False
@@ -269,6 +348,7 @@ def backward_substitution(n, a, b, o):
 
 
 def gauss_elimination(self, n, a, b):
+    self.runtime.set_start_time()
     o = [i for i in range(n)]
 
     if forward_elimination(self, n, a, b, o):
@@ -278,8 +358,12 @@ def gauss_elimination(self, n, a, b):
     else:
         print("There is no solution")
 
+    self.runtime.set_end_time()
+    self.runtime.show_runtime()
+
 
 def gauss_jordan(self, n, a, b):
+    self.runtime.set_start_time()
     o = [i for i in range(n)]
 
     if forward_elimination(self, n, a, b, o):
@@ -289,8 +373,13 @@ def gauss_jordan(self, n, a, b):
     else:
         print("There is no solution")
 
+    self.runtime.set_end_time()
+    self.runtime.show_runtime()
 
-def jacobi(n, a, b, initial_guess, epsilon, max_iteration):
+
+def jacobi(self, n, a, b, initial_guess, epsilon, max_iteration):
+    self.runtime.set_start_time()
+
     relative_error = [0 for _ in range(n)]
     x_new = [0 for _ in range(n)]
     x_old = initial_guess
@@ -318,8 +407,12 @@ def jacobi(n, a, b, initial_guess, epsilon, max_iteration):
     for i in range(n):
         print(x_new[i])
 
+    self.runtime.set_end_time()
+    self.runtime.show_runtime()
 
-def gauss_seidel(n, a, b, initial_guess, epsilon, max_iteration):
+
+def gauss_seidel(self, n, a, b, initial_guess, epsilon, max_iteration):
+    self.runtime.set_start_time()
     relative_error = [0 for _ in range(n)]
     x = initial_guess
 
@@ -344,9 +437,13 @@ def gauss_seidel(n, a, b, initial_guess, epsilon, max_iteration):
     for i in range(n):
         print(x[i])
 
+    self.runtime.set_end_time()
+    self.runtime.show_runtime()
+
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
