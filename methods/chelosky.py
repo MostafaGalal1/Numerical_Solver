259e31237293a007b567e9d355e238d8244b295f
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2022-12-21 02:29:00 +0200

    print

--- methods/chelosky.py
+++ methods/chelosky.py
@@ -10,10 +10,11 @@ class Chelosky(AbstractMethod):
         self.service = service
 
     def execute(self):
-        flag,ans = self.service.forward_elimination(self.n, self.a, [0 for _ in range(self.n)], [0 for _ in range(self.n)], True)
-        if not flag:
-           return "There is no solution"
-
+        x = Doolittle(self.n, self.a, self.b, self.service).execute()
+        if x == "There is no solution":
+            return x
+        elif x == "Infinite no of solutions":
+            return x
         l = [[0.0 for _ in range(self.n)] for _ in range(self.n)]
         u = [[0.0 for _ in range(self.n)] for _ in range(self.n)]
         d = [[0.0 for _ in range(self.n)] for _ in range(self.n)]
@@ -29,30 +30,8 @@ class Chelosky(AbstractMethod):
                 elif i > j:
                     l[i][j] = self.a[i][j]
 
-        ans += "L = \n" + "\n".join(
+        ans = "L = \n" + "\n".join(
             str(" , ".join(str(itt) for itt in l[it])) for it in range(self.n)) + "\n\nD = \n" + "\n".join(
             str(" , ".join(str(itt) for itt in d[it])) for it in range(self.n)) + "\n\nU = \n" + "\n".join(
-            str(" , ".join(str(itt) for itt in u[it])) for it in range(self.n)) + "\n\n"
-
-        x = GaussJordan(self.n, l, self.b, self.service).execute()
-        if x == "There is no solution":
-            return x
-        elif x == "Infinite no of solutions":
-            return x
-
-        ans += "\n\nY = " + " , ".join(str(it) for it in self.b)
-        x = GaussJordan(self.n, d, self.b, self.service).execute()
-        if x == "There is no solution":
-            return x
-        elif x == "Infinite no of solutions":
-            return x
-
-        ans += "\n\nZ = " + " , ".join(str(it) for it in self.b)
-        x = GaussJordan(self.n, u, self.b, self.service).execute()
-        if x == "There is no solution":
-            return x
-        elif x == "Infinite no of solutions":
-            return x
-
-        ans += "\n\nX = " + " , ".join(str(it) for it in self.b)
+            str(" , ".join(str(itt) for itt in u[it])) for it in range(self.n)) + "\n\nx = " + " , ".join(str(it) for it in self.b)
         return ans
--- methods/crout.py
+++ methods/crout.py
@@ -10,34 +10,38 @@ class Crout(AbstractMethod):
         self.service = service
 
     def execute(self):
-        flag,ans = self.service.forward_elimination(self.n, self.a, [0 for _ in range(self.n)], [0 for _ in range(self.n)], True)
-        if not flag:
-           return "There is no solution"
 
         l = [[0.0 for _ in range(self.n)] for _ in range(self.n)]
         u = [[0.0 for _ in range(self.n)] for _ in range(self.n)]
-        for i in range(self.n):
-            for j in range(self.n):
-                if i == j:
-                    u[i][j] = 1.0
-                    l[i][j] = self.a[i][j]
-                elif i > j:
-                    u[j][i] = self.a[i][j]
-                elif i < j:
-                    l[j][i] = self.a[i][j]
 
-        ans += "L = \n" + "\n".join(
+        for i in range(0, self.n):
+            u[i][i] = 1
+
+        for i in range(0, self.n):
+            for j in range(i, self.n):
+                tot = 0
+                for k in range(0, i):
+                    tot = tot + (l[j][k] * u[k][i])
+                l[j][i] = self.a[j][i] - tot
+            for j in range(i, self.n):
+                tot = 0
+                for k in range(0, i):
+                    tot = tot + (l[i][k] * u[k][j])
+                try:
+                    u[i][j] = (self.a[i][j] - tot) / l[i][i]
+                except ZeroDivisionError:
+                    return "There is no solution"
+
+        ans = "L = \n" + "\n".join(
             str(" , ".join(str(itt) for itt in l[it])) for it in range(self.n)) + "\n\nU = \n" + "\n".join(
-            str(" , ".join(str(itt) for itt in u[it])) for it in range(self.n)) + "\n\nx = " + " , ".join(str(it) for it in self.b)
-
+            str(" , ".join(str(itt) for itt in u[it])) for it in range(self.n)) + "\n\nx = " + " , ".join(
+            str(it) for it in self.b)
         x = GaussJordan(self.n, l, self.b, self.service).execute()
         if x == "There is no solution":
             return x
         elif x == "Infinite no of solutions":
             return x
-
         ans += "\n\nY = " + " , ".join(str(it) for it in self.b)
-        print(self.b)
         x = GaussJordan(self.n, u, self.b, self.service).execute()
         if x == "There is no solution":
             return x
