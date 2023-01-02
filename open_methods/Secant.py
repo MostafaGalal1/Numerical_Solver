f6418393d7232da4cc4a2f6a008b89c6a89632a9
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-01-02 08:50:10 +0200

    print

--- open_methods/Secant.py
+++ open_methods/Secant.py
@@ -12,29 +12,24 @@ class Secant(AbstractMethod):
     def execute(self):
         try:
             count = 1
-            lists = []
             f = lambda x: eval(self.function)
             x0 = self.x_lower
             x1 = self.x_upper
+            steps = ""
             while True:
-                list = []
                 x2 = self.service.apply_precision(x1 - self.service.apply_precision(f(x1) * (x1 - x0) / self.service.apply_precision(f(x1) - f(x0))))
                 if (x2 != 0 and self.service.apply_precision(abs(x2 - x1)/x2) < self.epsilon) or (count >= self.max_iteration):
                     break
-                list.append(count)
-                list.append(x0)
-                list.append(x1)
-                list.append(f(x0))
-                list.append(f(x1))
-                list.append(x2)
-                list.append(x2 - x1)
-                lists.append(list)
+                steps += "Iteration Number " + str(count) + ": \n"
+                steps += "Xi-1 = " + str(x0) + " | Xi = " + str(x1) + "\n"
+                steps += "f(Xi-1) = " + str(self.service.apply_precision(f(x0))) + " | f(Xi) = " + str(self.service.apply_precision(f(x1))) + "\n"
+                steps += "relative_error = " + str(self.service.apply_precision(abs(x1-x0)/x1)) + "\n"
+                steps += "________________________________\n"
                 x0 = x1
                 x1 = x2
-                print("the value of the root in iteration " + str(count) + " is " + str(x2))
                 count += 1
-            print(lists)
-            return lists
+            steps += "\n" + "the root of f(x) = " + str(x1) + "\n"
+            return steps
 
         except ZeroDivisionError:
             return "There is no solution"
